c9d3411c19871ce54d4ce36b7171313fffce45f3
Avickerman <Avickerm@gmail.com>
2018-11-24 17:52:37 -0600

    folders

---
+++ client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import $ from 'jquery';
+
+// import Hero from "../../components/";
+import "./LandingPage.css";
+// import API from "../../utils/API";
+
+
+
+
+
